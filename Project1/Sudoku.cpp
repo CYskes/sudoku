8443e2554dd446e476cca75a7ef04e108268c643
CYskes <connor.yskes@gmail.com>
2023-06-07 17:22:36 -0600

    06.07.2023 changes

--- Project1/Sudoku.cpp
+++ Project1/Sudoku.cpp
@@ -1,8 +1,13 @@
 #include "Sudoku.h"
 #include <iostream>
 #include <stdio.h>
+#include <string>
 
 #pragma warning(disable: 4996)
+
+const char* sudoku::entry() {
+    const char* selection{ " " };
+}
 
 void sudoku::runSudoku(bool mainRun)
 {
@@ -11,22 +16,8 @@ void sudoku::runSudoku(bool mainRun)
 
     squareOne();
 
-    ImGui::SetNextWindowSize(squareSize);
-    ImGui::SetNextWindowPos(ImVec2(windowHW * 1/3, 0));
-    ImGui::Begin("2", (bool*)mainRun, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoDecoration);
-    /*if (ImGui::BeginTable("2", 3, 0, squareSize)) {
-        for (int row = 0; row < 4; row++) {
-            ImGui::TableNextRow();
-            ImGui::TableNextColumn();
-            ImGui::Text("Row %d", row);
-            ImGui::TableNextColumn();
-            ImGui::Text("Some contents");
-            ImGui::TableNextColumn();
-            ImGui::Text("123.456");
-        }
-        ImGui::EndTable();
-    }*/
-    
+    squareTwo();
+
     ImGui::SetNextWindowSize(squareSize);
     ImGui::SetNextWindowPos(ImVec2(windowHW * 2/3, 0));
     ImGui::Begin("3", (bool*)mainRun, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
@@ -48,7 +39,7 @@ void sudoku::runSudoku(bool mainRun)
     ImGui::SetNextWindowPos(ImVec2(windowHW * 2/3, windowHW * 1/3));
     ImGui::Begin("6", (bool*)mainRun, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
 
-    ImGui::End();ImGui::End();ImGui::End(); ImGui::End(); ImGui::End();
+    ImGui::End();ImGui::End();ImGui::End(); ImGui::End();
 }
 
 
@@ -59,14 +50,35 @@ void sudoku::squareOne()
     ImGui::SetNextWindowPos(ImVec2(windowHW * 0, windowHW * 0));
     ImGui::Begin("1", (bool*)nullptr , ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
     //ImGui::SetNextWindowPos(ImVec2(0, 0));
-    ImGui::Button("1", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("2", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("3", ImVec2(100, 100));
-    ImGui::Button("4", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("5", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("6", ImVec2(100, 100));
-    ImGui::Button("7", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("8", ImVec2(100, 100));
-    ImGui::SameLine(); ImGui::Button("9", ImVec2(100, 100));
+    ImGui::Button("1", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("2", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("3", ImVec2(buttonHW, buttonHW));
+    ImGui::Button("4", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("5", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("6", ImVec2(buttonHW, buttonHW));
+    ImGui::Button("7", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("8", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("9", ImVec2(buttonHW, buttonHW));
+    ImGui::End();
+}
+
+void sudoku::squareTwo()
+{
+    ImGui::SetNextWindowSize(squareSize);
+    ImGui::SetNextWindowPos(ImVec2(windowHW * 1 / 3, windowHW * 0));
+    ImGui::Begin("2", (bool*)nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
+    if (ImGui::Button(sudoku::level1::box2[0], ImVec2(buttonHW, buttonHW))) {
+        sudoku::level1::box2[0] = "1";
+
+    }
+
+    ImGui::SameLine(); ImGui::Button("2", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("3", ImVec2(buttonHW, buttonHW));
+    ImGui::Button("4", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button(" ", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button(" ", ImVec2(buttonHW, buttonHW));
+    ImGui::Button("7", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("8", ImVec2(buttonHW, buttonHW));
+    ImGui::SameLine(); ImGui::Button("9", ImVec2(buttonHW, buttonHW));
     ImGui::End();
 }
--- Project1/Sudoku.h
+++ Project1/Sudoku.h
@@ -2,11 +2,27 @@
 
 namespace sudoku {
 	void runSudoku(bool mainRun);
-	void squareOne();
 
-	static float windowHW = 999;
+	void squareOne();
+	void squareTwo();
+	const char* entry();
+
+	static float windowHW = 666;
+	static float buttonHW = windowHW / 10.5;
 	static ImVec2 squareSize(windowHW / 3, windowHW / 3);
 	static ImVec2 iconSize(squareSize.x / 3, squareSize.x / 3);
 
+	namespace level1 {
+		static const char* box1[9]{ " " };
+		static const char* box2[9]{ " " };
+		static const char* box3[9]{ " " };
+		static const char* box4[9]{ " " };
+		static const char* box5[9]{ " " };
+		static const char* box6[9]{ " " };
+		static const char* box7[9]{ " " };
+		static const char* box8[9]{ " " };
+		static const char* box9[9]{ " " };
+
+	}
 }
 
